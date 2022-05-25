193da7ae74c3675a77ad0696152c58878c654ffe
HOD1151ME <98415785+HOD1151ME@users.noreply.github.com>
2022-05-25 19:31:36 +0530

    final natours design
    updated and final natours

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+let cl=console.log;
+
+const navigation=document.getElementById("navigation");
+const navToggle=document.getElementById("navToggle");
+
+navToggle.addEventListener('click', function(){
+    navigation.classList.toggle('active')
+})
